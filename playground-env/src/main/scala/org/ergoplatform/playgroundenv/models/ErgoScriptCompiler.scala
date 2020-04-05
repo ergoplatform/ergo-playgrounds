ce8de5604eb9350523efd6d0f96c73fa1f381b39
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2020-04-05 15:33:33 +0300

    lift env values into constants on ErgoScript compilation;

--- playground-env/src/main/scala/org/ergoplatform/playgroundenv/models/ErgoScriptCompiler.scala
+++ playground-env/src/main/scala/org/ergoplatform/playgroundenv/models/ErgoScriptCompiler.scala
@@ -5,15 +5,23 @@ import sigmastate.interpreter.Interpreter.ScriptEnv
 import sigmastate.lang.{SigmaCompiler, TransformingSigmaBuilder}
 import org.ergoplatform.ErgoAddressEncoder.TestnetNetworkPrefix
 import sigmastate.eval.CompiletimeIRContext
+import sigmastate.eval.Evaluation
+import sigmastate.SType
+import sigmastate.SType.AnyOps
 
 object ErgoScriptCompiler {
 
   val compiler = SigmaCompiler(TestnetNetworkPrefix, TransformingSigmaBuilder)
 
-  implicit var ctx: CompiletimeIRContext = new CompiletimeIRContext()
+  implicit var IR: CompiletimeIRContext = new CompiletimeIRContext()
 
   def compile(env: ScriptEnv, ergoScript: String): ErgoContract = {
-    val prop = compiler.compile(env, ergoScript)
+    val liftedEnv = env.mapValues { v =>
+      val tV      = Evaluation.rtypeOf(v).get
+      val elemTpe = Evaluation.rtypeToSType(tV)
+      IR.builder.mkConstant[SType](v.asWrappedType, elemTpe)
+    }
+    val prop = compiler.compile(liftedEnv, ergoScript)
     ErgoContract(_ => ???, prop)
   }
 }
