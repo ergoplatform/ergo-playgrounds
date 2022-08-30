33bf49ee7b9c3d8e74e86cf3ff4ef977eb3b457a
Tim Ling <791016+kettlebell@users.noreply.github.com>
2022-08-30 14:21:48 +1000

    Hash ergo-tree bytes only

--- playground-env/src/main/scala/org/ergoplatform/playgroundenv/utils/ErgoScriptCompiler.scala
+++ playground-env/src/main/scala/org/ergoplatform/playgroundenv/utils/ErgoScriptCompiler.scala
@@ -5,7 +5,7 @@ import sigmastate.interpreter.Interpreter.ScriptEnv
 import sigmastate.lang.{SigmaCompiler, TransformingSigmaBuilder}
 import org.ergoplatform.{ErgoAddressEncoder, Pay2SAddress}
 import org.ergoplatform.ErgoAddressEncoder.{MainnetNetworkPrefix, TestnetNetworkPrefix}
-import scorex.util.encode.{Base16, Base58};
+import scorex.util.encode.{Base16, Base64};
 import sigmastate.eval.CompiletimeIRContext
 import sigmastate.eval.Evaluation
 import sigmastate.SType
@@ -33,29 +33,23 @@ object ErgoScriptCompiler {
     implicit var encoder = new ErgoAddressEncoder(MainnetNetworkPrefix)
     var addr             = Pay2SAddress(ergoTree)
 
-    val addrTypePrefix = Pay2SAddress.addressTypePrefix
     val mainnetEncoded = encoder.toString(addr)
-    val mainnetHash = Base58.encode(
+    val ergoTreeHash = Base64.encode(
       ErgoAddressEncoder
-        .hash256((MainnetNetworkPrefix + addrTypePrefix).toByte +: ergoTree.bytes)
+        .hash256(ergoTree.bytes)
     )
 
     encoder = new ErgoAddressEncoder(TestnetNetworkPrefix)
     addr    = Pay2SAddress(ergoTree)
     val testnetEncoded = encoder.toString(addr)
-    val testnetHash = Base58.encode(
-      ErgoAddressEncoder
-        .hash256((TestnetNetworkPrefix + addrTypePrefix).toByte +: ergoTree.bytes)
-    )
 
     val serializedErgoTree = Base16.encode(ergoTree.bytes)
     println(
       s"Ergoscript @$url\n" +
       s"P2S(mainnet): $mainnetEncoded\n" +
-      s"P2S(mainnet hash): $mainnetHash\n" +
       s"P2S(testnet): $testnetEncoded\n" +
-      s"P2S(testnet hash): $testnetHash\n" +
-      s"Serialized ErgoTree: $serializedErgoTree"
+      s"Hashed ergo-tree bytes (base 64 encoded): $ergoTreeHash\n" +
+      s"Serialized ErgoTree (base 16 encoded): $serializedErgoTree"
     )
   }
 }
